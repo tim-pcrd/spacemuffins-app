2bff937d7d9b5fa63ede4c84667d12fbefee1b69
iamtimp <timp@tuta.io>
2019-11-29 00:17:31 +0100

    third commit

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports: [
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
